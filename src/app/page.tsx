838fab47ee11eb03c936db6b6d4d4f8a90798a31
Shoaib Jalal <syedshoaibjalal@outlook.com>
2024-07-12 14:22:12 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-gray-100 min-h-[80vh] md:w-[35%]">
       <h1 className='font-bold text-center text-3xl'>Todo App</h1>
-      
+      <p>input</p>
+      <p>map todos</p>
       </div>
   );
 }
